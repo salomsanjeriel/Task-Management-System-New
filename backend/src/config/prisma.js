70be0d70b578997629f3337d042f5edb1be5e9d9
jeriel-jr <jsalomsan@gmail.com>
2026-06-23 14:41:49 +0530

    feat: initialize Prisma client with pg adapter configuration

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,6 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+import * as prismaClientPkg from '@prisma/client';
+import * as adapterPgPkg from '@prisma/adapter-pg';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const prisma = new PrismaClient();
+const PrismaClient = prismaClientPkg.PrismaClient || prismaClientPkg.default?.PrismaClient;
+const PrismaPg = adapterPgPkg.PrismaPg || adapterPgPkg.default?.PrismaPg;
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
